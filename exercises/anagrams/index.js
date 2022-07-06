09ddca82af17f1cbd557d67c21bf8de4ca064012
gulnarhasanzada <gulnar.hesenzade@gmail.com>
2022-07-06 09:50:46 -0400

    Anagrams solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,30 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    let mapA = {};
+    let mapB = {};
+    let strA = stringA.replace(/[^\w]/g,"").toLowerCase()
+    let strB = stringB.replace(/[^\w]/g,"").toLowerCase()
+    for(let char of strA){
+        mapA[char] = mapA[char]+1 || 1;
+    }
+
+    for(let char of strB){
+        mapB[char] = mapB[char]+1 || 1;
+    }
+
+    if(Object.keys(mapA).length != Object.keys(mapB).length){
+        return false;
+    }
+
+    for(let el in mapA){
+        if(mapA[el] != mapB[el]){
+            return false;
+        }
+    }
+
+    return true;
+}
 
 module.exports = anagrams;
