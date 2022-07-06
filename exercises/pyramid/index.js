fa7562f9c6af1296744bdf8b2b28dec08adb5966
gulnarhasanzada <gulnar.hesenzade@gmail.com>
2022-07-06 11:17:00 -0400

    Pyramid solutions

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,35 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row=0, str="") {
+    // for (let i = 0; i < n; i++) {
+    //     let str ="";
+    //     for (let j = 0; j < 2*n-1; j++) {
+    //         if(j <= Math.floor((2*n-1)/2) +i && j >= Math.floor((2*n-1)/2)-i){
+    //             str+="#";
+    //         }else{
+    //             str+=" ";
+    //         }
+    //     } 
+    //     console.log(str)   
+    // }
+    
+    if(row === n){
+        return;
+    }
+
+    if(str.length === 2*n -1){
+        console.log(str)
+        return pyramid(n, row+1)
+    }
+
+    if(str.length <= Math.floor((2*n-1)/2) +row && str.length >= Math.floor((2*n-1)/2)-row){
+        str+="#";
+    }else{
+        str+=" ";
+    }
+
+    pyramid(n, row, str)
+}
 
 module.exports = pyramid;
